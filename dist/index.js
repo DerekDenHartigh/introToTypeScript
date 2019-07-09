5e69154d9a1f95d1585a2eab79dcb3c8cc6bda8d
Derek <derekdenhartigh@gmail.com>
2019-07-09 19:28:50 -0400

    compiled some ts into js, now downloading and using tslint

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var world = '';
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + word;
+}
+exports.hello = hello;
+;
+console.log(hello('world!'));
+//# sourceMappingURL=index.js.map
---
+++ readMe.md
@@ -0,0 +1,3 @@
+in terminal: "tsc --init", adds a tsconfig.json.
+"tsc" in the terminal compiles the Typescript to Javascript (as per the tsconfig.json, wither in root or another designated folder) 
+"tsc -w" starts a watcher
